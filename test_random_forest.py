cf2b335c58c0054ec852e9742437976b3b5428dc
Yossi Ben Shushan <yosib@fmr.co.il>
2019-06-03 16:12:08 +0300

    play with sklearn

--- test_random_forest.py
+++ test_random_forest.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 from random_forest import *
@@ -8,6 +9,14 @@ XX = (df.iloc[:, 2:]).values
 y = (df.iloc[:, 1] == 'M').values
 
 X_train, X_test, y_train, y_test = train_test_split(XX, y, test_size=0.30, random_state=70, shuffle=True)
-rf = RandomForest()
+
+rf = RandomForest(forest_size=5,max_depth=2)
 rf.train(X_train, y_train)
-print("Accuracy:{0:.2f}%".format(rf.test(X_test, y_test)))
+print("My Accuracy:{0:.2f}%".format(rf.test(X_test, y_test)))
+
+# use sklearn
+
+
+clf = RandomForestClassifier(n_estimators=5, max_depth=2, random_state=0)
+clf.fit(X_train, y_train)
+print("Sklearn Accuracy:{0:.2f}%".format(clf.score(X_test, y_test)*100))
